30096fe95b550983fa609d9f32d1f5cc5d1cffde
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-02-04 21:25:25 +0200

    add method removed from CinemaHallDaoImpl, MovieDaoImpl, MovieSessionDaoImpl

--- src/main/java/com/cinema/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/com/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 @Dao
 public class MovieSessionDaoImpl implements MovieSessionDao {
@@ -27,28 +26,6 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
                     .setParameter("to", toDateTime).getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Unable to get available MovieSessions from DB", e);
-        }
-    }
-
-    @Override
-    public MovieSession add(MovieSession movieSession) {
-        Transaction transaction = null;
-        Session session = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.save(movieSession);
-            transaction.commit();
-            return movieSession;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException("Unable to add " + movieSession + "to DB", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 }
